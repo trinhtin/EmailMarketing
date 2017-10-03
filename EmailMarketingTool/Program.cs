b6f8eb9aea34e362f3c5f16501d7be668b9403d9
Tran Minh Luan <15520454@gm.uit.edu.vn>
2017-10-03 08:46:29 +0700

    Init

---
+++ EmailMarketingTool/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace EmailMarketingTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //About author
+            Console.WriteLine("UIT Email Marketing Tool");
+            Console.WriteLine("Author: Phuc-Luan");
+
+            ////initialize infomation
+            //string toAddress = "";
+            //string fromAddress = "minhhoanghoangvo@gmail.com";
+            //string pass = "hoangvotong";
+            //string subject = "xin chao";
+            //string content = "hi";
+            //int IDNumber = 15520311;
+
+            //for (int i = 3; i <= 3; i++)
+            //{
+            //    //Gene  rate Email from ID 
+            //    toAddress = IDNumber.ToString() + "@gm.uit.edu.vn";
+
+            //    SendEmail.SendEmailMarketing(toAddress, fromAddress, pass, subject, content);
+            //    IDNumber++;
+            //}
+
+            SendEmail.Page_Load(null, EventArgs.Empty);
+
+
+        }
+
+    }
+}
---
+++ EmailMarketingTool/SendEmail.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailMarketingTool
+{
+    class SendEmail
+    {
+        public static void SendEmailMarketing(string toAddress,string fromAddress,string pass,string subject,string content ) 
+        {
+            using (MailMessage mail = new MailMessage())
+            {
+                //initialize info
+                mail.From = new MailAddress(fromAddress);
+                mail.To.Add(toAddress);
+                mail.Subject = subject;
+                mail.Body = content;
+                mail.IsBodyHtml = true;
+
+
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    //
+                    smtp.Credentials = new NetworkCredential(fromAddress, pass);
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
+        }
+        public static void sendHtmlEmail(string from_Email, string to_Email, string body, string from_Name, string Subject, string SMTP_IP, Int32 SMTP_Server_Port)
+        {
+            //create an instance of new mail message
+            MailMessage mail = new MailMessage();
+
+            //set the HTML format to true
+            mail.IsBodyHtml = true;
+
+            //create Alrternative HTML view
+            AlternateView htmlView = AlternateView.CreateAlternateViewFromString(body, null, "text/html");
+
+            //Add Image
+            LinkedResource theEmailImage = new LinkedResource("E:\\IMG_3332.jpg");
+            theEmailImage.ContentId = "myImageID";
+
+            //Add the Image to the Alternate view
+            htmlView.LinkedResources.Add(theEmailImage);
+
+            //Add view to the Email Message
+            mail.AlternateViews.Add(htmlView);
+
+            //set the "from email" address and specify a friendly 'from' name
+            mail.From = new MailAddress(from_Email, from_Name);
+
+            //set the "to" email address
+            mail.To.Add(to_Email);
+
+            //set the Email subject
+            mail.Subject = Subject;
+
+            //set the SMTP info
+            System.Net.NetworkCredential cred = new System.Net.NetworkCredential("minhhoanghoangvo@gmail.com", "hoangvotong");
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = cred;
+            //send the email
+            smtp.Send(mail);
+        }
+        public static void Page_Load(object sender, EventArgs e)
+        {
+            string Themessage = @"<html>
+                              <body>
+                                <table width=""100%"">
+                                <tr>
+                                    <td style=""font-style:arial; color:maroon; font-weight:bold"">
+                                   Hi! <br>
+                                    <img src=cid:myImageID>
+                                    </td>
+                                </tr>
+                                </table>
+                                </body>
+                                </html>";
+            sendHtmlEmail("minhhoanghoangvo@gmail.com", "cdtdmathoi@gmail.com", Themessage, "Scoutfoto", "Test HTML Email", "smtp.gmail.com", 25);
+        }
+    }
+}
